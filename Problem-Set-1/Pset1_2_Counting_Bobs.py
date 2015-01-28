d35ee0bc6a0ff6e4bd28c095baf0a78486f293c2
ankammer <kammermeierantje@googlemail.com>
2015-01-28 12:55:01 +0100

    Create Pset1_2_Counting_Bobs.py

---
+++ Problem-Set-1/Pset1_2_Counting_Bobs.py
@@ -0,0 +1,11 @@
+count = 0
+x = 0
+y = 1
+z = 2
+while (z < len(s)):
+    if s[x] == 'b' and s[y] == 'o' and s[z] == 'b':
+        count += 1
+    x += 1
+    y += 1
+    z += 1
+print("Number of times bob occurs is: " + str(count))
